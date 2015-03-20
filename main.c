8e7fa1b983f125809ae5feb5cb3d5424e687844a
OdnaropX <colecionador.gabriel@gmail.com>
2015-03-19 21:33:20 -0300

    Fix count

--- main.c
+++ main.c
@@ -3,38 +3,34 @@
 #include <math.h>
 #include <time.h>
 
-double estima_pi(int pc, int pq){
-	double pi;
-	//pi = (pc/pq)/4
-	//4 * pc = pq * pi
-	pi =  (double) pc / pq;
-	return pi;
-}
-
-int main(){
-	int i, pc, pq, init_x, init_y;
+double estima_pi(int entrada){
+	int pc, i;
 	double x, y, pi;
-	srand48(time(NULL));
-
-	init_x = 1;
-	init_y = 1;
-	pq = 0;
+	
 	pc = 0;
 
-	for(i = 0; i< 10000; i++){
+	for(i = 0; i < entrada; i++){
 		x = drand48();
 		y = drand48();
 		if(i == 0) {
 			printf("First value %f, %f", x, y);
 		}
-		if(sqrt(x * x + y * y) > 1){
-			pq++;
-		}
-		else {
+		if(sqrt(x * x + y * y) <= 1){
 			pc++;
 		}
 	}
-	pi = estima_pi(pc, pq);
+
+	pi = (double) (pc * 4) / entrada;
+
+	return pi;
+
+}
+
+int main(){
+	double pi;
+	srand48(time(NULL));
+
+	pi = estima_pi(10000);
 	printf("pi %f", pi);
 	return 0;
 }
